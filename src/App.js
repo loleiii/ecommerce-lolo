1a72ec331e0f865b877450d23735084b2fd4f2bc
loleiii <59238339+loleiii@users.noreply.github.com>
2020-12-10 21:25:25 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import CartWidget from 'CartWidget()'
+function App() {
+  return (
+    <header>
+      <div className="parteSuperior"><h4>¡10% OFF EN TODO CON CODIGO REACT!</h4></div>
+      <div className="navSuperior">
+        <img src="./public/kikuyo.png" alt="logo"/>
+        <input className="inputBusqueda" type="text" placeholder="¿Que estas buscando?"></input>
+      <div className="carrito">
+          <CartWidget />
+
+          <div className="cuenta">Tu cuenta</div>
+
+      </div>
+
+      </div>
+      <div>
+        <ul className="nav">
+          <li className="itemNav">ITEM1</li>
+          <li className="itemNav">ITEM2</li>
+          <li className="itemNav">ITEM3</li>
+          <li className="itemNav">ITEM4</li>
+          <li className="itemNav">ITEM5</li>
+          <li className="itemNav">ITEM6</li>
+          <li className="itemNav">ITEM7</li>
+          <li className="itemNav">ITEM8</li>
+
+        </ul>
+      </div>
+    </header>
+
+  )
+}
+
+export default App;
---
+++ src/CartWidget.js
@@ -0,0 +1,11 @@
+import React from "react"
+const CartWidget() => {
+  return (
+    <CartWidget>
+    <FontAwesomeIcon icon= "shopping-cart" />
+    </CartWidget>
+
+  )
+}
+
+export default CartWidget;
